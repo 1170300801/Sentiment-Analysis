3b5fb42bf77b72d68120877c29b5ab337db58e19
Mirakal <zehao323@gmail.com>
2019-04-09 18:42:08 +0800

    4/9/2019

---
+++ analysis.py
@@ -0,0 +1,87 @@
+#coding:utf-8
+import math
+import sys  
+import jieba
+reload(sys)  
+sys.setdefaultencoding('utf8')   
+f = open('wordic.txt')
+#pos = open('possentence.txt','wb')
+#neg = open('negsentence.txt','wb')
+#neu = open('neusentence.txt','wb')
+p = open('wordicout.txt')
+w = open('result.txt','wb')
+fpos = open('pnn_annotated.txt')
+dic = []
+dicout = []
+coutpos = 0
+coutneg = 0
+coutneu = 0
+
+'''
+for line in fpos:
+    line = line.strip('\n')
+    num = int(line[0:2])
+    line = line[2:]
+    if num == 0:
+        neu.write(line + '\n')
+        coutneu += 1
+    elif num == 1:
+        pos.write(line + '\n')
+        coutpos += 1
+    else:
+        neg.write(line[1:] + '\n')
+        coutneg += 1
+'''
+#pos.close
+#neg.close
+#neu.close
+
+pos = open('possentence.txt')
+neg = open('negsentence.txt')
+neu = open('neusentence.txt')
+
+for line in f:   
+    dic.append(line.strip('\n'))
+
+for line in p:
+    dicout.append(int(line.strip('\n')))
+
+ration = 0
+
+for line in pos:
+    total = 0
+    words = jieba.cut(line)
+    for word in words:
+        if word in dic:
+            total += dicout[dic.index(word)]
+
+    if total >= 3:
+        ration += 1
+
+print('pos correct ration: ', ration/50.0)
+
+ration = 0
+for line in neg:
+    total = 0
+    words = jieba.cut(line)
+    for word in words:
+        if word in dic:
+            total += dicout[dic.index(word)]
+
+    if total <= -3:
+        ration += 1
+
+print('neg correct ration: ', ration/50.0)
+
+ration = 0
+for line in neu:
+    total = 0
+    words = jieba.cut(line)
+    for word in words:
+        if word in dic:
+            total += dicout[dic.index(word)]
+
+    if total > -3 and total < 3:
+        ration += 1
+
+print('neu correct ration: ', ration/50.0)
---
+++ test.py
@@ -0,0 +1,60 @@
+#coding:utf-8
+import math
+import jieba
+import sys  
+reload(sys)  
+sys.setdefaultencoding('utf8')   
+fpos = open('pnn_annotated.txt')
+dicstop = open('stopwords.dic')
+dictotal = []
+dic1 = []
+dic2 = []
+dicstop = []
+dic = []
+
+cout = 0
+for line in fpos:
+    line = line.strip('\n')
+    num = int(line[0:2])
+    line = line[2:]
+    words = jieba.cut(line)
+    for word in words:
+        if word not in dicstop and num > 0:
+            dic1.append(word)
+            dictotal.append(word)
+        elif word not in dicstop and num < 0:
+            dic2.append(word)
+            dictotal.append(word)
+
+f = open('wordic.txt','wb')
+p = open('wordicout.txt','wb')
+dic = list(set(dictotal))
+
+for i in range(len(dic)):
+    total = dic1.count(dic[i]) + dic2.count(dic[i])
+    differ = dic1.count(dic[i]) - dic2.count(dic[i])
+    weight = int(pow(3, float(pow(abs(differ), 1.0/3) * abs(differ)) / total))
+    
+    if total == 1:
+        f.write(dic[i] + '\n')
+        p.write(str(differ) + '\n')
+
+    elif differ > 1:
+        f.write( dic[i] + '\n')
+        if weight > 1:
+            p.write(' ' + str(weight) + '\n')
+        else:
+            p.write('0' + '\n')
+
+    
+    elif differ < -1:
+        f.write(dic[i] + '\n')
+        if weight > 1:
+            p.write(' -' + str(weight) + '\n')
+        else:
+            p.write('0' + '\n')
+
+
+
+f.close
+p.close
